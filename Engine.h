3c33ee6f4a8294d35ad772fa6a23769e2008ea77
Artur <faizrahmanov.artur@mail.ru>
2021-11-25 15:14:00 +0300

    Wrapper and engine done

--- Engine.h
+++ Engine.h
@@ -1,30 +1,24 @@
 #pragma once
 #include "Wrapper.h"
-#include <string>
-#include <map>
-
-/// <summary>
-/// Engine class stores the names of the commands and the wrappers for them. 
-/// It allows you to create a new command based on a method of some class or function. 
-/// It also allows you to call the appropriate command.
-/// </summary>
 class Engine
 {
+public:
+	void registerCommand(Wrapper* wrapper, std::string commandName)
+	{
+		if (commands.find(commandName) != commands.end())
+			throw std::exception("Command already exists");
+
+		commands.insert(std::make_pair(commandName, wrapper));
+	}
+	int execute(std::string commandName, std::map<std::string, int> args)
+	{
+		auto const& command = commands.find(commandName);
+
+		if (command == commands.end())
+			throw std::exception("Command doesn't exist");
+
+		return command->second->funcExecute(args);
+	}
 private:
 	std::map<std::string, Wrapper*> commands;
-public:
-	/// <summary>
-	/// 
-	/// </summary>
-	/// <param name="wrapper">a wrapper for a function that stores information about it</param>
-	/// <param name="commandName">new command name</param>
-	void registerCommand(Wrapper* wrapper, std::string commandName);
-
-	/// <summary>
-	/// 
-	/// </summary>
-	/// <param name="commandName"></param>
-	/// <param name="arguments"></param>
-	/// <returns></returns>
-	int execute(std::string commandName, std::map<std::string, int> arguments);
 };
--- Wrapper.h
+++ Wrapper.h
@@ -1,19 +1,56 @@
+#pragma once
 #include <functional>
-#include <string>
 #include <vector>
 #include <map>
+#include <iostream>
+#include <functional>
 
 class Wrapper
 {
-private:
-	std::map<std::string, int> argsAndDefValues;
-	std::function<int(std::vector<int> const&)> func;
 public:
-	template<class baseClass, class... arguments>
-	Wrapper(baseClass* instance, int(baseClass::* method)(arguments...), std::vector<std::pair<std::string, int>> defaltValues);
+	template <typename baseClass, typename... Args>
+	Wrapper(baseClass* instance, int(baseClass::* method)(Args...), std::vector<std::pair<std::string, int>> arguments)
+	{
+		if (sizeof...(Args) != arguments.size())
+			throw std::exception("too few or too many arguments");
 
-	template<class... arguments>
-	Wrapper(int(*function)(arguments...), std::vector<std::pair<std::string, int>> defaltValues);
+		for (auto const& arg : arguments)
+		{
+			this->arguments.insert(arg);
+		}
 
-	int call(std::map<std::string, int> mapArguments);
+		func = [this, instance, method](std::vector<int> args)
+		{
+			return (call(instance, method, args, std::make_index_sequence<sizeof...(Args)>{}));
+		};
+	}
+
+	int funcExecute(std::map<std::string, int> arguments)
+	{
+		std::vector<int> args;
+
+		if (arguments.size() != this->arguments.size())
+			throw std::exception("too few or too many arguments");
+
+		for (auto const& arg : this->arguments)
+		{
+			auto const& value = arguments.find(arg.first);
+			if (value == arguments.end())
+				throw std::exception("argument not found");
+
+			args.push_back(value->second);
+		}
+
+		return func(args);
+	}
+
+private:
+	std::map<std::string, int> arguments;
+	std::function<int(std::vector<int>)> func;
+
+	template<typename baseClass, typename Meth, size_t... Is>
+	int call(baseClass* bc, Meth method, std::vector<int> argVec, std::index_sequence<Is...>)
+	{
+		return ((bc->*method)(argVec[Is]...));
+	}
 };
